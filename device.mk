followmsi/android_device_google_dragon

Copy vendor seccomp policy to vendor partion
Bug: 34723744
Test: Dragon builds and boots.
Test: Watch cat videos on youtube no seccomp violations observed.
Test: For both mediacodec and mediaextractor verify
"cat proc/<pid>/status | grep Seccomp" == "Seccomp: 2"

Change-Id: I5c1c7fa0facd9afc32ccc2437bb9d2a9641d5d95

device.mk
@@ -293,4 +293,8 @@ PRODUCT_PACKAGES += \
     rmi4update \
     rmihidtool
 
+# Vendor seccomp policy files for media components:
+PRODUCT_COPY_FILES += \
+    device/google/dragon/seccomp_policy/mediacodec.policy:$(TARGET_COPY_OUT_VENDOR)/etc/seccomp_policy/mediacodec.policy
+
 $(call inherit-product-if-exists, vendor/nvidia/dragon/dragon-vendor.mk)

GZOSP-Devices/device_htc_flounder

device.mk
@@ -363,6 +363,10 @@ PRODUCT_PROPERTY_OVERRIDES += \
     af.fast_track_multiplier=1 \
     audio_hal.period_size=128
 
+# Vendor seccomp policy files for media components:
+PRODUCT_COPY_FILES += \
+    device/htc/flounder/seccomp_policy/mediacodec.policy:$(TARGET_COPY_OUT_VENDOR)/etc/seccomp_policy/mediacodec.policy
+
 # In userdebug, add minidebug info the the boot image and the system server to support
 # diagnosing native crashes.
 ifneq (,$(filter userdebug, $(TARGET_BUILD_VARIANT)))